5874278c821325796dbd2a3ea79965f7ddfa9de8
Zahrotul <zahrotuljannah693@gmail.com>
2024-04-04 06:03:58 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click",function video(){
+    alert("ini video My Litle  Pony");
+});
+
+Audio.addEventListener("click",function audio(){
+    alert("ini audio My Litle  Pony");
+});
+
+gambar.addEventListener("click",function gambar(){
+    alert("ini gambar My Litle  Pony");
+});
